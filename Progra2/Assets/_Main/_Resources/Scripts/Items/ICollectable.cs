c147c7290f23d27bc0adb6be67c5b0d82b2e5a3c
Tomi <63859937+tomicancheff@users.noreply.github.com>
2022-08-30 13:57:45 -0300

    TileMap Changes, Camera Changes
    +TilePalette for props and buildings
    +Scripts for health Controller, Game manager,  and ICollectable
    > Fixed Cinemachine Camera that follows player and has new boundaries now.
    --------------------------------------
    TileMap needs fix  ( sorting layers of sprites)

---
+++ Progra2/Assets/_Main/_Resources/Scripts/Items/ICollectable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICollectable 
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
