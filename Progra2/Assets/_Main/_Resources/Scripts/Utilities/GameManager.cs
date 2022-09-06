7c56421e82fef85fc86b6f57fb63a4db3ffea938
Tomi <63859937+tomicancheff@users.noreply.github.com>
2022-09-06 12:33:50 -0300

    Added PLayer Attack Animations,  Item Script
    Potion,Crystals and keys are pickable.
    
    NEEDS  workaround, UI HUD  feedback and Inventory system  ( lists/ dictionary)

--- Progra2/Assets/_Main/_Resources/Scripts/Utilities/GameManager.cs
+++ Progra2/Assets/_Main/_Resources/Scripts/Utilities/GameManager.cs
@@ -6,17 +6,20 @@ public class GameManager : MonoBehaviour
 {
     //SINGLETON TYPE
     // Enemies or other classes´s health should work apart from this script, this works exclusively for the Player
-
+  
     public static GameManager gameManager { get; private set; }
-
+  
     //Referencing the Player´s Health controller
     public HealthController _playerHealth = new HealthController(100, 200);
+
+
 
     void Awake()
     {
         if (gameManager != null && gameManager != this)
         {
-            Destroy(this);
+            
+            DontDestroyOnLoad(this);
         }
         else
         {
@@ -25,4 +28,5 @@ public class GameManager : MonoBehaviour
     }
 
    
+
 }
