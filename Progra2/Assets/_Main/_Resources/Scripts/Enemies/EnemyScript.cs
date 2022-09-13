2f5539dc7818cdb1154878cd868a77fcc7a2cf46
Tomi <63859937+tomicancheff@users.noreply.github.com>
2022-09-13 15:21:57 -0300

    Added Enemy Chase and lineOf Sight

--- Progra2/Assets/_Main/_Resources/Scripts/Enemies/EnemyScript.cs
+++ Progra2/Assets/_Main/_Resources/Scripts/Enemies/EnemyScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Enemy variables
+    [SerializeField] private float speed;  
+    private Transform player;
+    [SerializeField] private float lineOfSight;
+    private float distance;
+    
+    //METHODS--------------------------------
     void Start()
     {
-        
+        //Gets the reference to the player with a Tag.
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //Checks if the Player´s in range, and then chase it.
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+
+        if(distanceFromPlayer < lineOfSight)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+          
+    }
+
+    //Gizmos for the enemy Line of sight view
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSight);
+
     }
 }
